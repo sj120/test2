67eaf399aa6b1c4c16abe6fa8306e1c82cbe892d
sj120 <sana120@naver.com>
2023-04-05 11:10:29 +0900

    first commit

---
+++ Tests01/src/ch2/VarEx4.java
@@ -0,0 +1,12 @@
+package ch2;
+
+public class VarEx4 {
+
+	public static void main(String[] args) {
+	
+		 float f = 0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f;
+		 
+		 System.out.println(f);
+	}
+
+}
